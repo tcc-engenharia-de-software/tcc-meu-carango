3c033edc3f9558db1364b7ada67622d135b3c0e1
glaucioripol <glaucioripol@gmail.com>
2023-05-22 10:29:37 -0300

    chore: install and done setup of navigation

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@ import { StatusBar } from "expo-status-bar";
 import { NativeWindStyleSheet } from "nativewind";
 import { SafeAreaView } from "react-native";
 
-import { Bootstrap } from "./src";
+import { EntryPoint } from "./src";
 
 NativeWindStyleSheet.setOutput({ default: "native" });
 
 export default () => (
   <SafeAreaView style={{ flex: 1, marginTop: Constants.statusBarHeight }}>
     <StatusBar style="dark" />
-    <Bootstrap />
+    <EntryPoint />
   </SafeAreaView>
 );
