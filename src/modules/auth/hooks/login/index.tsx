0d9c79ecc8390296f2de2c926f239b4b8578b79a
glaucioripol <glaucioripol@gmail.com>
2023-05-24 14:33:48 -0300

    feat: initial form setup

--- src/modules/auth/hooks/login/index.tsx
+++ src/modules/auth/hooks/login/index.tsx
@@ -1,3 +1,6 @@
+import { useMemo } from "react";
+import { useForm } from "react-hook-form";
+
 import { RootStackScreenProps } from "../../../../shared";
 
 type UseLoginControllerProps = {
@@ -6,8 +9,8 @@ type UseLoginControllerProps = {
 
 /*
   ! todo:
-  - [ ] install react-hook-form
-  - [ ] do logic to handle with form using react-hook-form
+  - [x] install react-hook-form
+  - [x] do logic to handle with form using react-hook-form
   - [ ] install supabase client
   - [ ] implement authentication with supabase
   - [ ] install lib to handle with async storage
@@ -17,8 +20,73 @@ type UseLoginControllerProps = {
   - [ ] create tests
   - [ ] move types to separate file
 */
+const initialFormValues = { email: "", password: "" };
 export const useLoginController = ({ navigation }: UseLoginControllerProps) => {
+  const { control, handleSubmit, formState, reset } = useForm<LoginFormData>({
+    defaultValues: initialFormValues,
+    mode: "onChange",
+    // resolver: undefined,
+  });
+
+  const onSubmit = handleSubmit((data: LoginFormData) => {
+    console.log(data);
+
+    reset(initialFormValues);
+    navigation.push("Home");
+  });
+
+  const handleInputChange = (cb: (text: string) => void) => {
+    return (currentText: string) => {
+      cb(currentText.trim());
+    };
+  };
+
+  const isButtonSubmitDisabled =
+    !formState.isValid || formState.isSubmitting || formState.isDirty;
+
+  const emailError = useMemo(() => {
+    if (formState.errors.email?.type === "pattern") {
+      return "Digite um email válido";
+    }
+
+    if (formState.errors.email?.type === "minLength") {
+      return "Email deve ter no mínimo 8 caracteres";
+    }
+
+    if (formState.errors.email?.type === "maxLength") {
+      return "Email deve ter no máximo 120 caracteres";
+    }
+
+    return null;
+  }, [formState.errors.email]);
+
+  const passwordError = useMemo(() => {
+    if (formState.errors.password?.type === "minLength") {
+      return "Senha deve ter no mínimo 8 caracteres";
+    }
+
+    if (formState.errors.password?.type === "maxLength") {
+      return "Senha deve ter no máximo 120 caracteres";
+    }
+
+    return null;
+  }, [formState.errors.password]);
+
   return {
-    onSubmit: () => navigation.push("Home"),
+    formControl: control,
+    isLoading: formState.isSubmitting,
+    isButtonSubmitDisabled,
+    onSubmit,
+    handleInputChange,
+    emailError,
+    passwordError,
   };
 };
+
+// it should be in a separate to be used as model
+type LoginFormData = {
+  email: string;
+  password: string;
+};
+
+export type UseLoginController = ReturnType<typeof useLoginController>;
---
+++ src/modules/auth/views/login/styles.ts
@@ -0,0 +1,14 @@
+export const styles = {
+  container: "bg-white flex flex-1 px-4 pt-14",
+  title: "text-6xl mb-8",
+  form: {
+    inputWrapper: "mb-4",
+    input: "border-black border-2 text-3xl w-full px-4 py-2",
+    inputError: "border-red-500",
+    inputErrorText: "text-red-500",
+    submitButton: "py-2",
+    submitButtonEnabled: "bg-black",
+    submitButtonDisabled: "bg-gray-500",
+    submitButtonText: "text-3xl text-white text-center font-bold",
+  },
+};
