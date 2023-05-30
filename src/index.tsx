a78f1885908c718d8650778194186df2d83e6fd6
ViniciusBarth <viniciusbarthhh@hotmail.com>
2023-05-30 19:16:25 -0300

    zzz

--- src/index.tsx
+++ src/index.tsx
@@ -3,30 +3,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { FC } from "react";
 
 import { LoginController, SignUpController, HomeController } from "src/modules";
-import { Text, TouchableOpacity, View } from "react-native";
 
-import { RootStackParamList, SCREEN_NAMES } from "./shared";
-
-const Page1: FC<RootStackParamList["Home"]> = ({ navigation }) => {
-  return (
-    <View className="bg-purple-100 flex-1 gap-4">
-      <Text className="text-xl">
-        Open up App.js to{" "}
-        <Text className="text-red-600 font-bold">start working</Text> on your
-        app!
-      </Text>
-      <Text className="text-xl">
-        Changes you make will automatically reload.
-      </Text>
-
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Login")}
-        className="bg-red-500 py-4 px-8 rounded-xl self-center w-4/5">
-        <Text className="text-white text-center">Click me</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+import { SCREEN_NAMES } from "./shared";
 
 const Stack = createNativeStackNavigator();
 
