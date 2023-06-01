a3dafd1d39756e0ef9b1db46c1449c6f03cb8869
ViniciusBarth <viniciusbarthhh@hotmail.com>
2023-05-31 22:39:26 -0300

    zzz

--- src/modules/auth/views/home/index.tsx
+++ src/modules/auth/views/home/index.tsx
@@ -1,22 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
-<<<<<<< HEAD
 import { Header } from "src/components/Header";
 import { NewVehicle } from "src/components/NewVehicle";
-=======
->>>>>>> main
 
 export const HomeView: React.FC = () => {
   return (
     <View>
-<<<<<<< HEAD
       <Header title="Meu carango" description="Veículos" />
       <NewVehicle />
-=======
-      <Text>
-        Open up App.js to <Text>start working</Text> on your app!
-      </Text>
->>>>>>> main
     </View>
   );
 };
