2b017fc1d296b45a70eb92a8051a0328cb448b04
ViniciusBarth <viniciusbarthhh@hotmail.com>
2023-08-02 22:13:18 -0300

    Update index.tsx

--- src/components/ResponseInfo/index.tsx
+++ src/components/ResponseInfo/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Button, Text, View } from "react-native";
 
-import { styles } from "./styles";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 interface ResponseInfoError {
