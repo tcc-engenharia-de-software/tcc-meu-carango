b298b013fbac4c52c5c428fc13a986e792313977
ViniciusBarth <viniciusbarthhh@hotmail.com>
2023-08-12 09:27:09 -0300

    feat(menu): add menu component

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { styles } from "./styles";
-import { useIsOpenMenu } from "~/hooks/use-is-open-menu";
+import { useIsOpenMenu } from "./../../hooks/use-is-open-menu";
+import { Menu } from "../Menu";
 
 export const TopBar: React.FC = () => {
   const { setIsOpen, isOpen } = useIsOpenMenu();
@@ -12,14 +13,23 @@ export const TopBar: React.FC = () => {
   };
 
   return (
-    <View className={styles.container}>
-      <TouchableOpacity onPress={handleOpenMenu}>
-        <Icon name="menu" color="#717171" size={20} />
-      </TouchableOpacity>
+    <View>
+      <View className={styles.container}>
+        <TouchableOpacity onPress={handleOpenMenu}>
+          {!isOpen ? (
+            <Icon name="menu" color="#717171" size={20} />
+          ) : (
+            <Icon name="close" color="#717171" size={20} />
+          )}
+        </TouchableOpacity>
 
-      <TouchableOpacity>
-        <Icon name="bell-outline" color="#717171" size={20} />
-      </TouchableOpacity>
+        <TouchableOpacity>
+          <Icon name="bell-outline" color="#717171" size={20} />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Menu isOpen={isOpen} />
+      </View>
     </View>
   );
 };
