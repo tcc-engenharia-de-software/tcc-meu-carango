65767c7b4259641193fc8bb05855784ded0f2944
glaucio <glaucioripol@gmail.com>
2023-08-13 09:13:30 +0100

    feat: done response info

--- src/components/ResponseInfo/index.tsx
+++ src/components/ResponseInfo/index.tsx
@@ -1,7 +1,8 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { SCREEN_NAMES } from "src/shared";
 
 interface ResponseInfoError {
   title: string;
@@ -14,24 +15,52 @@ interface ResponseInfoError {
 export const ResponseInfo: React.FC<ResponseInfoError> = ({
   title,
   description,
-  actionBackHome,
-  actionTryAgain,
+  actionBackHome = () => {},
+  actionTryAgain = () => {},
   error = false,
 }) => {
+  const navigation = useNavigation();
+
   const handleBackHome = () => {
     actionBackHome();
+    navigation.navigate(SCREEN_NAMES.Home as never);
   };
+
   const handleTryAgain = () => {
     actionTryAgain();
   };
 
   return (
-    <View className="">
-      <Text>{title}</Text>
-      <Icon name="close" />
-      <Text>{description}</Text>
-      <Button title="Ir para tela principal" onPress={handleBackHome} />
-      {error && <Button title="Tente novamente" onPress={handleTryAgain} />}
+    <View className="flex flex-1 p-4">
+      <Text className="text-[36px]">{title}</Text>
+
+      <View className="flex flex-row justify-center my-6">
+        {error ? (
+          <Image source={require("../../../assets/responseInfo/error.png")} />
+        ) : (
+          <Image source={require("../../../assets/responseInfo/success.png")} />
+        )}
+      </View>
+
+      <Text className="text-xl">{description}</Text>
+
+      <TouchableOpacity
+        onPress={handleBackHome}
+        className="bg-black py-2 rounded-md mt-4">
+        <Text className="text-xl text-white text-center font-bold p-2">
+          Ir para tela principal
+        </Text>
+      </TouchableOpacity>
+
+      {error && (
+        <TouchableOpacity
+          onPress={handleTryAgain}
+          className="border-black border-2 py-2 rounded-md mt-3">
+          <Text className="text-xl text-center font-bold p-2">
+            Tente novamente
+          </Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
